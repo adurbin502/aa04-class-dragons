408b1fa96eeb22639ee955feed5074a4f8278ed1
Andrew <adurbin502@gmail.com>
2024-03-07 20:56:45 -0500

    intial commit

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,15 @@
-// Your code here 
+const Dragon = require("./Dragon");
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+}
+
+try {
+    module.exports = FriendlyDragon;
+  } catch {
+    module.exports = null;
+  }
